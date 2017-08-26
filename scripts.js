1946d7433ca4425cd5f6b81b1e5431327398061d
banganga <alex_5454@list.ru>
2017-08-26 09:26:57 +0500

    alex add new object and so on

--- scripts.js
+++ scripts.js
@@ -10,6 +10,14 @@ function countNum (num1) {
 	return res;
 }
 
+var dog = {};
+dog.name = 'Good boy';
+dog.age = 3;
+dog.dick_length = 20;
+console.log("My dog's name is: " + dog.name + ", and it " + dog.age + " years old.");
+console.log("He has " + dog.dick_length + "cm dick");
+delete dog.dick_length;
+console.log("Aaaaaannnndddd now he hasn't : " + dog.dick_length);
 /* ====================== max ======================== */
 var object = {
 
@@ -18,5 +26,5 @@ object.name = "Вася";
 object.surname = "Петров";
 object.name = "Сергей";
 delete object.name;
-alert(object.name);
+console.log("And then in the darkness of the night Max shouted: " + object.name);
 
